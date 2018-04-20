3f85c8f608e57bb2e98f84c590d5c5e8d3530864
Preston Burns <Preston_Burns@rhoworld.com>
2018-04-20 12:27:37 -0400

    modularized

--- src/onLayout.js
+++ src/onLayout.js
@@ -1,65 +1,11 @@
 import createNestControl from './onLayout/createNestControl';
 import drawLegend from './onLayout/drawLegend';
+import applyFilters from './onLayout/applyFilters';
 import flattenData from './flattenData';
 
-function clone(obj) {
-    let copy;
-
-    //boolean, number, string, null, undefined
-    if ('object' != typeof obj || null == obj) return obj;
-
-    //date
-    if (obj instanceof Date) {
-        copy = new Date();
-        copy.setTime(obj.getTime());
-        return copy;
-    }
-
-    //array
-    if (obj instanceof Array) {
-        copy = [];
-        for (var i = 0, len = obj.length; i < len; i++) {
-            copy[i] = clone(obj[i]);
-        }
-        return copy;
-    }
-
-    //object
-    if (obj instanceof Object) {
-        copy = {};
-        for (var attr in obj) {
-            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
-        }
-        return copy;
-    }
-
-    throw new Error('Unable to copy [obj]! Its type is not supported.');
-}
-
-//import filter from './onLayout/filter';
-function applyFilters() {
-    //If there are filters, return a filtered data array of the raw data.
-    //Otherwise return the raw data.
-
-    this.data.filtered_ = this.filters  //need to make this unique for the if in flattenData
-        ? clone(this.data.initial).filter(d => {
-              let match = true;
-              this.filters.forEach(filter => {
-                  if (match === true && filter.val !== 'All')
-                      match =
-                          filter.val instanceof Array
-                              ? filter.val.indexOf(d[filter.col]) > -1
-                              : filter.val === d[filter.col];
-              });
-              return match;
-          })
-        : clone(this.data.initial);
-}
-
 export default function onLayout() {
-    var table = this;
+    var chart = this;
     var selects = this.controls.wrap.selectAll('select');
-
 
     selects.on('change', function() {
         // Get the selected levels
@@ -69,16 +15,15 @@ export default function onLayout() {
         });
 
         // Update filters to reflect the selected levels
-        table.filters.forEach(function(d, i) {
+        chart.filters.forEach(function(d, i) {
             d.val = selectedfilterLevels[i];
         });
 
-        applyFilters.call(table);
-        table.data.raw = flattenData.call(table);
-
-        table.draw();
+        applyFilters.call(chart);
+        chart.data.raw = flattenData.call(chart);
+        chart.draw();
     });
 
-    createNestControl.call(table);
-    drawLegend.call(table);
+    createNestControl.call(chart);
+    drawLegend.call(chart);
 }
