32614a50c549638aad961d9cddc5b011ff8b3e89
Preston Burns <Preston_Burns@rhoworld.com>
2018-06-01 15:57:48 -0400

    started adding filters to apply filters

--- build/raveXplorer.js
+++ build/raveXplorer.js
@@ -582,7 +582,9 @@ function onClick(th, header) {
     }
 }
 
-function filterData(data) {}
+function filterData(data) {
+  // going to do this in applyfilters...
+}
 
 function filterable() {
     return {
@@ -720,6 +722,16 @@ function applyFilters() {
 
     //If there are filters, return a filtered data array of the raw data.
     //Otherwise return the raw data.
+
+    // add filters from filterable
+    this.filterable.filters.forEach(function (filter) {
+        _this.filters.push({
+            col: filter.col,
+            val: filter.level
+        });
+    });
+
+    console.log(this.filters); //these are wrong!!
 
     this.data.filtered_ = this.filters //need to make this unique for the if in flattenData
     ? clone(this.data.initial).filter(function (d) {
--- src/onLayout/applyFilters.js
+++ src/onLayout/applyFilters.js
@@ -4,6 +4,16 @@ export default function applyFilters() {
     //If there are filters, return a filtered data array of the raw data.
     //Otherwise return the raw data.
 
+// add filters from filterable
+    this.filterable.filters.forEach(filter => {
+      this.filters.push({
+          col: filter.col,
+          val: filter.level
+});
+});
+
+console.log(this.filters) //these are wrong!!
+
     this.data.filtered_ = this.filters //need to make this unique for the if in flattenData
         ? clone(this.data.initial).filter(d => {
               let match = true;
@@ -17,4 +27,6 @@ export default function applyFilters() {
               return match;
           })
         : clone(this.data.initial);
+
+
 }
