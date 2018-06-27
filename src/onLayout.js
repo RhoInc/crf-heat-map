72c07f5d8ce7bb5365e36195ee2fca1eafa7b736
samussiah <samussiah@gmail.com>
2018-06-27 16:08:41 -0400

    move exports to top

--- src/onLayout.js
+++ src/onLayout.js
@@ -1,11 +1,13 @@
 import customizeFilters from './onLayout/customizeFilters';
 import createNestControl from './onLayout/createNestControl';
+import moveExportButtons from './onLayout/moveExportButtons';
 import drawLegend from './onLayout/drawLegend';
 import addColumnControls from './onLayout/addColumnControls';
 
 export default function onLayout() {
     customizeFilters.call(this);
     createNestControl.call(this);
+    moveExportButtons.call(this);
     drawLegend.call(this);
     addColumnControls.call(this);
 }
