04b16f6ad34150f74a0f8119a0230fb5cc2d49fb
samussiah <samussiah@gmail.com>
2018-06-28 16:35:31 -0400

    add defineLayout.js

---
+++ src/defineLayout.js
@@ -0,0 +1,39 @@
+export default function defineLayout() {
+    this.containers = {
+        main: d3.select(this.element)
+            .append('div')
+            .attr('id', 'crf-heat-map'),
+    };
+
+    /**-------------------------------------------------------------------------------------------\
+	  Left column
+    \-------------------------------------------------------------------------------------------**/
+
+        this.containers.leftColumn = this.containers.main
+            .append('div')
+            .classed('chm-column chm-column--left', true);
+
+            this.containers.dataExport = this.containers.leftColumn
+                .append('div')
+                .classed('chm-row chm-row--1 chm-data-export', true);
+
+            this.containers.controls = this.containers.leftColumn
+                .append('div')
+                .classed('chm-row chm-row--2 chm-controls chm-controls--main', true);
+
+    /**-------------------------------------------------------------------------------------------\
+	  Right column
+    \-------------------------------------------------------------------------------------------**/
+
+        this.containers.rightColumn = this.containers.main
+            .append('div')
+            .classed('chm-column chm-column--right', true);
+
+            this.containers.nestControls = this.containers.rightColumn
+                .append('div')
+                .classed('chm-row chm-row--1 chm-controls chm-controls--nests', true);
+
+            this.containers.table = this.containers.rightColumn
+                .append('div')
+                .classed('chm-row chm-row--2 chm-table', true);
+}
