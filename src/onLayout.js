6f1baa01ed0b26551fe20ae6f0b16b5848e4dc9e
samussiah <samussiah@gmail.com>
2018-07-13 14:55:08 -0400

    make subject status dropdown multi-select

--- src/onLayout.js
+++ src/onLayout.js
@@ -1,10 +1,12 @@
 import customizeFilters from './onLayout/customizeFilters';
+import tweakMultiSelects from './onLayout/tweakMultiSelects';
 import customizeCheckboxes from './onLayout/customizeCheckboxes';
 import moveExportButtons from './onLayout/moveExportButtons';
 import addColumnControls from './onLayout/addColumnControls';
 
 export default function onLayout() {
     customizeFilters.call(this);
+    tweakMultiSelects.call(this);
     customizeCheckboxes.call(this);
     //moveExportButtons.call(this);
     addColumnControls.call(this);
