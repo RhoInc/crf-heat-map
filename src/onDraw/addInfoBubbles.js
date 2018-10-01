b908cd9306cde61368e1d077b5d9a9b2f80323d0
Preston Burns <Preston_Burns@rhoworld.com>
2018-10-01 17:12:28 -0400

    update version to minor release

--- src/onDraw/addInfoBubbles.js
+++ src/onDraw/addInfoBubbles.js
@@ -2,14 +2,15 @@ export default function addInfoBubbles() {
     const chart = this;
 
     const infoMapping = {
-        is_partial_entry: "Data have been submitted in the EDC system.",
-        verified: "All required fields have source data verification complete in EDC.",
-        ready_for_freeze: "All required cleaning is complete (e.g. SDV, queries resolved) and data are ready to be frozen in EDC.",
-        is_frozen: "Data have been frozen in the EDC system.",
-        is_signed: "Data have been signed in the EDC system.",
-        is_locked: "Data have been locked in the EDC system.",
-        open_query_ct: "Site has not responded to issue.",
-        answer_query_ct: "Site has responded to issue, DM needs to review."
+        is_partial_entry: 'Data have been submitted in the EDC system.',
+        verified: 'All required fields have source data verification complete in EDC.',
+        ready_for_freeze:
+            'All required cleaning is complete (e.g. SDV, queries resolved) and data are ready to be frozen in EDC.',
+        is_frozen: 'Data have been frozen in the EDC system.',
+        is_signed: 'Data have been signed in the EDC system.',
+        is_locked: 'Data have been locked in the EDC system.',
+        open_query_ct: 'Site has not responded to issue.',
+        answer_query_ct: 'Site has responded to issue, DM needs to review.'
     };
 
     // add info bubbles and either info text, if defined, or the name of variable
