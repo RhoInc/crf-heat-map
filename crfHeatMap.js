eaebcd683e10168b198e07f0a9ecfa16eab386d1
samussiah <samussiah@gmail.com>
2019-05-21 14:20:10 -0400

    faster destruction

--- crfHeatMap.js
+++ crfHeatMap.js
@@ -2971,11 +2971,11 @@
         //remove stylesheet
         this.parent.style.remove();
 
-        //clear container
-        d3
-            .select(this.parent.element)
-            .selectAll('*')
-            .remove();
+        //clear container, removing one child node at a time (fastest method per https://jsperf.com/innerhtml-vs-removechild/37)
+        var node = d3.select(this.parent.element).node();
+        while (node.firstChild) {
+            node.removeChild(node.firstChild);
+        }
     }
 
     function checkRequiredVariables() {
--- src/onDestroy.js
+++ src/onDestroy.js
@@ -2,9 +2,7 @@ export default function onDestroy() {
     //remove stylesheet
     this.parent.style.remove();
 
-    //clear container
-    d3
-        .select(this.parent.element)
-        .selectAll('*')
-        .remove();
+    //clear container, removing one child node at a time (fastest method per https://jsperf.com/innerhtml-vs-removechild/37)
+    const node = d3.select(this.parent.element).node();
+    while (node.firstChild) node.removeChild(node.firstChild);
 }
